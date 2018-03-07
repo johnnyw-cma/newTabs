9d1cd40ce79220fdb0a84dbce76e42ba96d39aba
myislandwifi <34301761+myislandwifi@users.noreply.github.com>
2018-03-07 13:40:44 -0600

    fix android playback

--- src/pages/audio-capture/audio-capture.ts
+++ src/pages/audio-capture/audio-capture.ts
@@ -1,8 +1,8 @@
 //import { NativeAudio } from '@ionic-native/audio';
-import {Component, ViewChild} from '@angular/core';
-import {IonicPage, NavController, Platform} from 'ionic-angular';
-import {Media, MediaObject } from '@ionic-native/media';
-import {File} from '@ionic-native/file';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, Platform } from 'ionic-angular';
+import { Media, MediaObject } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
 
 
 //import {Camera} from '@ionic-native/camera';
@@ -14,39 +14,45 @@ import {File} from '@ionic-native/file';
   providers: []
 })
 export class AudioPage {
-
-  getAudioList() {
-    if(localStorage.getItem("audiolist")) {
-      this.audioList = JSON.parse(localStorage.getItem("audiolist"));
-      console.log(this.audioList);
-    }
-  }
-
   @ViewChild('myaudio')
   recording: boolean = false;
   filePath: string;
   fileName: string;
   audio: MediaObject;
   audioList: any[] = [];
+  logMessages: any[] = [];
 
   constructor(public navCtrl: NavController,
-              public platform: Platform,
-              private file: File,
-              private media: Media) {
+    public platform: Platform,
+    public file: File,
+    private media: Media) {
+  }
+
+  getAudioList() {
+    if (localStorage.getItem("audiolist")) {
+      this.audioList = JSON.parse(localStorage.getItem("audiolist"));
+      console.log(this.audioList);
+    }
   }
 
   startRecord() {
-    if (this.platform.is('ios')) {
-      this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.m4a';
-      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
-      this.audio = this.media.create(this.filePath);
-    } else if (this.platform.is('android')) {
-      this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.3gp';
-      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
-      this.audio = this.media.create(this.filePath);
+    try {
+      if (this.platform.is('ios')) {
+        this.fileName = 'record' + new Date().getDate() + new Date().getMonth() + new Date().getFullYear() + new Date().getHours() + new Date().getMinutes() + new Date().getSeconds() + '.m4a';
+        this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+        this.audio = this.media.create(this.filePath);
+      } else if (this.platform.is('android')) {
+        this.fileName = 'record' + new Date().getDate() + new Date().getMonth() + new Date().getFullYear() + new Date().getHours() + new Date().getMinutes() + new Date().getSeconds() + '.3gp';
+
+        this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+        this.audio = this.media.create(this.filePath);
+      }
+
+      this.audio.startRecord();
+      this.recording = true;
+    } catch (e) {
+      alert('exception playing audio:' + e)
     }
-    this.audio.startRecord();
-    this.recording = true;
   }
 
   stopRecord() {
@@ -57,16 +63,31 @@ export class AudioPage {
     this.recording = false;
     this.getAudioList();
   }
-  
-  playAudio(file,idx) {
-    if (this.platform.is('ios')) {
-      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
-      this.audio = this.media.create(this.filePath);
-    } else if (this.platform.is('android')) {
-      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
-      this.audio = this.media.create(this.filePath);
+
+  playAudio(filePath, idx) {
+    try {
+      this.logMessage('playing')
+      if (this.platform.is('ios')) {
+        this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + filePath;
+        this.audio = this.media.create(this.filePath);
+      } else if (this.platform.is('android')) {
+        this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + filePath;
+        this.audio = this.media.create(this.filePath);
+      }
+      let duration = this.audio.getDuration();
+      this.logMessage('duration:' + duration)
+      //this.audio.onStatusUpdate.subscribe(status => console.log(status)); // fires when file status changes
+      //this.audio.onSuccess.subscribe(() => console.log('Action is successful'));
+      this.audio.onError.subscribe(error => alert('Error!' + error));
+
+      this.audio.play();
+      this.audio.setVolume(0.8);
+    } catch (e) {
+      this.logMessage('exception playing audio:' + e)
     }
-    this.audio.play();
-    this.audio.setVolume(0.8);
+  }
+
+  logMessage(s) {
+    this.logMessages.push('log:' + s)
   }
 }
