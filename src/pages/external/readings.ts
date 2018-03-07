909667fbcc659ffa2c297a68444902bc2bda1dfc
myislandwifi <34301761+myislandwifi@users.noreply.github.com>
2018-03-06 22:51:26 -0600

    changed the way cheerio is imported, removed it as a constructor parameter and refactored a function out of the constructor
    changed the way cheerio is imported, removed it as a constructor parameter and refactored a function out of the constructor

--- src/pages/external/readings.ts
+++ src/pages/external/readings.ts
@@ -2,43 +2,45 @@ import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import * as rp from 'request-promise';
 //import { InAppBrowser } from '@ionic-native/in-app-browser';
-import { Cheerio } from 'cheerio';
+import * as cheerio from 'cheerio'
 
 @Component({
-  selector: 'page-readings',
-  templateUrl: 'readings.html'
+	selector: 'page-readings',
+	templateUrl: 'readings.html'
 })
 export class ReadingsPage {
 	public start_up: any;
 	public readings_url: string;
 
-	constructor(private cheerio: Cheerio,
-							public navCtrl: NavController,
-							private platform: Platform) {
+	constructor(
+		public navCtrl: NavController,
+		private platform: Platform) {
 
-  	//var readings_url = "https://crystalmeth.org/cma-meetings/start-a-meeting/meeting-readings/category/3-cma-readings.html";
-  	this.readings_url = 'https://crystalmeth.org/cma-meetings/start-a-meeting/meeting-readings/category/3-cma-readings.html';
-  	//var target = "_system";
-    //var options = "location=yes,toolbar=yes";
+		//var readings_url = "https://crystalmeth.org/cma-meetings/start-a-meeting/meeting-readings/category/3-cma-readings.html";
+		this.readings_url = 'https://crystalmeth.org/cma-meetings/start-a-meeting/meeting-readings/category/3-cma-readings.html';
+		//var target = "_system"; 
+		//var options = "location=yes,toolbar=yes";
 		//const browser = iab.create(readings_url, target, options);
-		
-		function gettHtmlContent(uri) {
-			var options = {
-					method: 'GET',
-					uri: uri,
-			};
-			return rp(options)
-					.then(function (resp) {
-							const $ = cheerio.load(resp);
-							var htmlString = $('body').html();
-							return htmlString;
-					})
-					.catch(function (err) {
-							return err;
-					});
-	}	
+
 		platform.ready().then(() => {
-			gettHtmlContent(this.readings_url);
+			let resp = this.gettHtmlContent(this.readings_url);
+			console.log(resp)
 		})
+	}
+
+	gettHtmlContent(uri) {
+		var options = {
+			method: 'GET',
+			uri: uri,
+		};
+		return rp(options)
+			.then(function (resp) {
+				const $ = this.cheerio.load(resp);
+				var htmlString = $('body').html();
+				return htmlString;
+			})
+			.catch(function (err) {
+				return err;
+			});
 	}
 }
