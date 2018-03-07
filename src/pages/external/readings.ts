c2007e24dd44abd850c1ec334f6187c41e385a0a
Johnny W <johnny.w@cmagso.net>
2018-03-06 22:41:47 -0600

    cheerio import fix
    yah

--- src/pages/external/readings.ts
+++ src/pages/external/readings.ts
@@ -2,21 +2,19 @@ import { Component } from '@angular/core';
 import { NavController, Platform } from 'ionic-angular';
 import * as rp from 'request-promise';
 //import { InAppBrowser } from '@ionic-native/in-app-browser';
-import { Cheerio } from 'cheerio';
+import * as cheerio from 'cheerio';
 
 @Component({
   selector: 'page-readings',
   templateUrl: 'readings.html'
 })
 export class ReadingsPage {
-	public start_up: any;
+	readingsHtmlContent: any;
 	public readings_url: string;
 
-	constructor(private cheerio: Cheerio,
-							public navCtrl: NavController,
+	constructor(public navCtrl: NavController,
 							private platform: Platform) {
 
-  	//var readings_url = "https://crystalmeth.org/cma-meetings/start-a-meeting/meeting-readings/category/3-cma-readings.html";
   	this.readings_url = 'https://crystalmeth.org/cma-meetings/start-a-meeting/meeting-readings/category/3-cma-readings.html';
   	//var target = "_system";
     //var options = "location=yes,toolbar=yes";
@@ -36,9 +34,10 @@ export class ReadingsPage {
 					.catch(function (err) {
 							return err;
 					});
-	}	
+		}
+
 		platform.ready().then(() => {
-			gettHtmlContent(this.readings_url);
+			this.readingsHtmlContent = gettHtmlContent(this.readings_url);
 		})
 	}
 }
