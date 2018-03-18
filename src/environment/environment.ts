0287d637ae0d1b8e4976565c4b5845e56d886933
Johnny W <johnny.w@cmagso.net>
2018-03-18 07:49:55 -0500

    updates for Firebase

---
+++ src/environment/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey: "AIzaSyDe2ro_E_W-xGix1y4pNIwNcmV_iytRCN4",
+      authDomain: "newtabs-6aaf6.firebaseapp.com",
+      databaseURL: "https://newtabs-6aaf6.firebaseio.com",
+      projectId: "newtabs-6aaf6",
+      storageBucket: "newtabs-6aaf6.appspot.com",
+      messagingSenderId: "661116740985"
+    },
+    meetingReadings: {
+        areYouATweaker: "Are_You_a_Tweaker.pdf",
+        whatIsCMA2017: "What_id_CMA_2017.pdf",
+        the12StepsPlanAction: "The_Twelve_Steps-A_Plan_of_Action_2017.pdf",
+        iCanStaySober: "I_Can_Stay_Sober.pdf"
+       }
+  };
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as firebase from 'firebase';
+
+import { environment } from '../../environment/environment';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  tweaker;
 
   constructor(public navCtrl: NavController) {
-
+    firebase.initializeApp(environment.firebase);
+    this.getSomeText();
   }
 
+  getSomeText() {
+      firebase.storage().ref().child('Are_You_a_Tweaker.pdf').getDownloadURL()
+      .then(response => this.tweaker = response)
+      .catch(error => console.log('error', error))
+  };
 }
